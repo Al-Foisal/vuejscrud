0ee82524073be2b541291099163cfbc73e00578d
Md Hafiz Al Foisal <alfoisal940700@gmail.com>
2023-07-20 23:59:33 +0600

    login navegation added

--- resources/js/component/App.vue
+++ resources/js/component/App.vue
@@ -2,8 +2,11 @@
     <header
         class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"
     >
-        <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"
-            >Company name</a
+        <router-link
+            v-if="!isLogin"
+            :to="{ name: 'login' }"
+            class="navbar-brand col-md-3 col-lg-2 me-0 px-3"
+            >Login</router-link
         >
 
         <div class="navbar-nav" v-if="isLogin">
